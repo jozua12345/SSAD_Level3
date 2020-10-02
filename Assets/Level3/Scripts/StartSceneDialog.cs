bfe38458892ba7383963d766cebcac54f1c859b9
jozua12345 <jozua12345@outlook.com>
2020-10-02 21:19:14 +0800

    Added start scene

---
+++ Assets/Level3/Scripts/StartSceneDialog.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogManager : MonoBehaviour {
+    private Queue<string> sentences;
+
+    void Start() {
+        sentences = new Queue<string>();
+    }
+
+}
